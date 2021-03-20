b3795926640a6fdd9a32e602b2c7377145c7b216
Paul Dudley <p4ttch@gmail.com>
2021-03-20 21:18:43 +0200

    first push, getting started

---
+++ main.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+# objectives.
+    # rename a file with specific name.
+    # move a file from 1 folder to another.
+
+# windows root= C:\
+# mac & linux root= /
+# starting location / file path
+startingPath = Path('C:\\','Users','p4ttch','Desktop','target-1')
+endingPath = Path('C:\\','Users','p4ttch','Desktop','target-2')
+
+# startingPath = "C:\Users\p4ttch\Desktop\target-1"
+# ending location / file path
+
+print(startingPath, endingPath)
